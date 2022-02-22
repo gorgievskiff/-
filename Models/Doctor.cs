d71a765ae910e4b05578e3c83be71ee7953bc194
Ѓорѓиевски Филип <filip.gjorgjievski@students.finki.ukim.mk>
2022-02-22 15:18:46 +0100

    Add project files.

---
+++ Models/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace МојТермин.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public ICollection<Patient> Patients { get; set; }
+
+
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace МојТермин.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public Doctor Doctor { get; set; }
+
+
+    }
+}
